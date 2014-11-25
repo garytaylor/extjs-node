edbd3cdde06789f85608fd18c83712ac95b07ef4
Gary Taylor <gary.taylor@hismessages.com>
2014-11-25 14:07:24 +0000

    Corrected paths and now copying over the downloaded uncompressed folder to 'ext'.  Had problems with permissions when moving, so just left 2 copies in there for now. (in a rush !!)

--- install.js
+++ install.js
@@ -2,28 +2,40 @@ var Download = require('download');
 var VERSION = '4.2.0'
 var path = require('path');
 var fs = require('fs');
+var fse = require('fs-extra');
 var destFolder = path.resolve('./src');
 var DecompressZip = require('decompress-zip');
-console.log('Downloading from http://cdn.sencha.io/ext-' + VERSION + '-gpl.zip');
+console.log('Downloading from http://cdn.sencha.com/ext/gpl/ext-' + VERSION + '-gpl.zip');
 var download = new Download()
     .get('http://cdn.sencha.com/ext/gpl/ext-' + VERSION + '-gpl.zip', path.resolve('./'), { extract: false});
-
 download.run(function (err) {
     var unzipper;
     if (err) {
         throw err;
     }
     fs.renameSync(path.join('./', 'ext-' + VERSION + '-gpl.zip'), path.join('./', 'ext.zip'));
-    if(fs.existsSync(path.join(destFolder, 'ext'))){
-        fs.rmdirSync(path.join(destFolder, 'ext'));
+    if(fs.existsSync(destFolder)){
+        fse.removeSync(destFolder);
     }
     unzipper = new DecompressZip(path.resolve('./ext.zip'))
     unzipper.on('error', function (err) {
         console.log('Caught an error ' + err.message);
     });
     unzipper.on('extract', function (log) {
-        fs.renameSync(path.join(destFolder, 'extjs-' + VERSION), path.join(destFolder, 'ext'));
-        console.log('Download complete');
+        var folderName;
+        debugger;
+        folderName = fs.readdirSync(destFolder).filter(function (name) {return name.match(new RegExp('^ext-' + VERSION))})[0];
+        if(!folderName) {
+            throw new Error('Cannot find the unzipped folder');
+        }
+        setTimeout(function () {
+            console.log('Copying from "' + path.join(destFolder, folderName) + '" to "' + path.join(destFolder, 'ext') + '"');
+            fse.copy(path.join(destFolder, folderName), path.join(destFolder, 'ext'), function (err) {
+                if (err) return console.error(err);
+                //fse.removeSync(path.join(destFolder, folderName));
+                console.log('Download complete');
+            });
+        }, 1000);
     });
     unzipper.extract({
         path: destFolder,
